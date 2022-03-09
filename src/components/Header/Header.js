f4c2b14fc3a3acc58a50fe5a81fb341bb07139ab
Ricardo <rr155914@gmail.com>
2022-03-09 12:57:43 -0600

    Add styling to header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,15 @@ import "./Header.css"
 const Header = () => {
   return (
     <form className="form" id="form"> 
-    <input className="input" type="search" id="query" name="q" placeholder="Search..." />
+    <div className="container-input">
+     <input className="input" type="search" id="query" name="q" placeholder="Search..." />
+      <img className="bell" src={require("./bell.png")} alt="Icon" />
+      <img className="avatar" src={require("./Ellipse93.png")} alt="Icon" />
+    </div>
+    <div className="Switcher">
+    <img className="bell" src={require("./Swicter.png")} alt="Icon" />
+    <img className="button-header" src={require("./Button.png")} alt="Icon" />
+    </div>
     </form>
   )
 }
